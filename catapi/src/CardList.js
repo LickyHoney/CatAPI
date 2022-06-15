1324722e83aa2a8914f59095921b3baf68563b45
LickyHoney <45100031+LickyHoney@users.noreply.github.com>
2022-06-15 12:36:15 +0300

    List all breeds

--- catapi/src/CardList.js
+++ catapi/src/CardList.js
@@ -1,11 +1,39 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const CardList = (props) => {
+  // Declarations
+  const [breeds, setBreeds] = useState([]);
+
+  // Fetching the breeds data using useEffect hook.
+
+  useEffect(() => {
+    axios.get(`https://api.thecatapi.com/v1/breeds`).then((res) => {
+      console.log(res);
+      const data = res.data;
+
+      setBreeds(data);
+    });
+  }, []);
+
+  //Rendering the web page.
+
   return (
     <div className="tc">
       <header id="myheader" className="header-dark">
         <h1 className="w3-sans-serif">Cat Breeds</h1>
       </header>
+      <div className="flex">
+        {breeds.map((breed, i) => {
+          return (
+            <>
+              <ul key={breed.id}>
+                <li>{breed.name}</li>
+              </ul>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
